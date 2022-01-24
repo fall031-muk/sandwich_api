e7ed634a8f82e8cc1be97cfe69a7020f7a62b7a8
fall031-muk <fall900802@gmail.com>
2022-01-24 15:50:22 +0900

    modeling

--- sandwich/models.py
+++ sandwich/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
-# Create your models here.
+class Sandwich(models.Model):
+    bread   = models.ForeignKey('Bread', on_delete=models.CASCADE)
+    topping = models.ForeignKey('Topping', on_delete=models.CASCADE)
+    cheese  = models.ForeignKey('Cheese', on_delete=models.CASCADE)
+    source  = models.ForeignKey('Source', on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'sandwiches'
+
+class Bread(models.Model):
+    name          = models.CharField(max_length=30)
+    quantity_left = models.PositiveIntegerField(default=0)
+    price         = models.PositiveIntegerField(default=0)
+
+    class Meta:
+        db_table = 'breads'
+
+class Topping(models.Model):
+    name          = models.CharField(max_length=30)
+    quantity_left = models.PositiveIntegerField(default=0)
+    price         = models.PositiveIntegerField(default=0)
+
+    class Meta:
+        db_table = 'toppings'
+
+class Cheese(models.Model):
+    name          = models.CharField(max_length=30)
+    quantity_left = models.PositiveIntegerField(default=0)
+    price         = models.PositiveIntegerField(default=0)
+
+    class Meta:
+        db_table = 'cheeses'
+
+class Source(models.Model):
+    name          = models.CharField(max_length=30)
+    quantity_left = models.PositiveIntegerField(default=0)
+    price         = models.PositiveIntegerField(default=0)
+
+    class Meta:
+        db_table = 'sources'
